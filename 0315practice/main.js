713f7dafd1e6765a9c78c619cd16fdfb2cbd86ee
tom23785886 <74181737+tom23785886@users.noreply.github.com>
2021-10-11 13:59:23 +0800

    Add files via upload

---
+++ 0315practice/main.js
@@ -0,0 +1,14 @@
+window.onload=function(){
+    var btn=document.getElementById("btn");
+    btn.addEventListener("click",function(){
+        document.body.style.background=randColor();
+    })
+}
+function randColor(){
+    var hex=[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+    return '#'+(function co(lor) {
+        return (lor += hex[Math.floor(Math.random()*16)]) && (lor
+            ?lor
+            :co(lor);
+    })('');
+}
